1eed42bbaf62cbc6111baf0d91b056f33da10de8
meenajainshah <meena.shah@iviewlabs.com>
2025-08-06 21:43:17 +0530

    added chat router and gpt router

--- app/memory_server.py
+++ app/memory_server.py
@@ -2,8 +2,24 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
 from routers.memory_router import router as memory_router
+from fastapi import FastAPI
+from routers.gpt_router import router as gpt_router
+from routers.chat_router import router as chat_router
+
 
 app = FastAPI()
+
+app = FastAPI()
+
+# Include all routers here
+app.include_router(memory_router)
+app.include_router(gpt_router)
+app.include_router(chat_router)
+
+@app.get("/")
+def root():
+    return {"message": "Chroma memory + GPT API is running!"}
+
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
